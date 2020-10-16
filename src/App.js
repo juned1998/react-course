854d314c7178f50a8a995815c7c2f729d5b8cda6
Juned Khan <junaidkhanhere@gmail.com>
2020-10-16 20:50:10 +0530

    react js usestate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Person from "./Person/Person";
+import "./App.css";
 
-function App() {
+const App = props => {
+  const [personsState, setPersonState] = useState({
+    persons : [
+      { name : 'Max', age: 20},
+      { name : 'John', age: 29},
+      { name : 'Stephanie', age: 30}
+    ]
+  });
+
+  const [otherValue, changeOtherValue] = useState("Some Value");
+
+  console.log(otherValue);
+
+
+  const switchNameHandler = () => {
+    // alert("hello");
+    // DON'T DO THIS  this.state.persons[0].name = "Rock";
+    setPersonState({
+      persons: [
+        { name : "Jerry", age: 21 },
+        { name : "Tom", age: 22 },
+        { name : "Chandler", age: 25 },
+      ],
+    });
+
+    changeOtherValue("NEw Other Value");
+  }
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobby is reading books.</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
     </div>
+    //React.createElement('div', {className:"App"}, React.createElement('h1',null, 'he12llo'))
   );
+  
 }
 
 export default App;
+
+
+
